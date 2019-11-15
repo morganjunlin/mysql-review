70a71c358b7c7ef3fc825e374c7425bd95c62b03
morganl92 <morganlinbox@gmail.com>
2019-11-14 18:38:27 -0800

    [feat] completes step1

--- database/index.js
+++ database/index.js
@@ -1,3 +1,19 @@
 const mysql = require('mysql');
 
 // TODO: establish connection
+var db = mysql.createConnection({
+  user     : 'root',
+  password : 'password',
+  database : 'ReviewDB'
+});
+
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected DB');
+});
+
+module.exports = db;
