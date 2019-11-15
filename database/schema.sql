9ffec568da0ab604d598d6c9d9198c3ceef2c847
morganl92 <morganlinbox@gmail.com>
2019-11-14 21:55:29 -0800

    Revert "[feat] creates schema file"
    This reverts commit 64a5b33049c9a71a6491ab705d5f436169b5e252.

--- database/schema.sql
+++
@@ -1,9 +0,0 @@
-USE ReviewDB;
-
-CREATE TABLE Notebook (
-  id int auto_increment not null primary key,
-  person varchar(255),
-  thing varchar(255)
-);
-
-/* run this file by running the command `mysql -u root -p < schema.sql` */
