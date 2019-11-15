1bc73dcf22ebbb0b362b721a029ff2104e4c3b1d
morganl92 <morganlinbox@gmail.com>
2019-11-14 21:17:55 -0800

    [feat] completes step4

--- server/controllers.js
+++ server/controllers.js
@@ -1,10 +1,23 @@
+const dbHelpers = require('../database/dbHelpers.js')
+
 module.exports = {
   getStuff: (req, res) => {
-    console.log(req.body)
-    res.send('Hello World')
+    dbHelpers.getAllTasks((err, results) => {
+      if (err) {
+        res.status(404).send('Error getting stuff from database', err)
+      } else {
+        res.status(200).send(results)
+      }
+    })
   },
 
-  postStuff: (req, res) => {
-    res.send(`Hi, I'm post request`)
+  postStuff: ({ body }, res) => {
+    dbHelpers.postOneTask(body, (err) => {
+      if (err) {
+        res.status(400).send('Error posting task in DB', err)
+      } else {
+        res.status(201).send(`Successfully added task ${body.task} to DB`)
+      }
+    })
   }
 }
